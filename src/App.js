223a61dd7a408f1be080130ce92e8ba31c9ae27c
Serhii-Halenko <galenkosergij830@gmail.com>
2025-08-02 20:25:06 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import Header from './components/header';
 import './App.css';
-import "./components/header.css"
-import iconButton from './images/box.png';
+import './components/header.css';
+import boxIcon from './images/box.png';
+
+function IconButton({ onClick, alt = "Box", ...props }) {
+  return (
+    <button
+      type="button"
+      aria-label="box button"
+      className="icon-button"
+      onClick={onClick}
+      {...props}
+    >
+      <img src={boxIcon} alt={alt} />
+    </button>
+  );
+}
+
 function App() {
   return (
     <>
       <Header />
       <main>
         <section id="home">Game content</section>
-        <section id="Game"></section>
+        <div className="button-container">
+          <IconButton onClick={() => console.log("Icon button clicked")} />
+        </div>
+        <section id="game" />
       </main>
     </>
   );
 }
 
 export default App;
-
